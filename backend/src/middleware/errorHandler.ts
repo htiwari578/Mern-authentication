6d10a2c79935a77d9a2f4697436dcc95824458ec
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-14 23:33:11 +0530

    updated code

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler, Response } from "express";
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
 import { z } from "zod";
+import AppError from "../utils/AppError";
 
 
 const handleZodError = (res:Response , error: z.ZodError) => {
@@ -12,7 +13,14 @@ const handleZodError = (res:Response , error: z.ZodError) => {
     message:error.message,
     errors,
   })
-}
+};
+
+const  handleAppError = (res: Response , error: AppError) => {
+  return res.status(error.statusCode).json({
+    message: error.message,
+    errorCode: error.errorCode,
+  });
+};
 
 
 // Error handler middleware
@@ -22,6 +30,11 @@ const errorHandler: ErrorRequestHandler = (error ,req, res, next )=> {
 
   if(error instanceof z.ZodError){
     return handleZodError(res , error);
+  }
+
+  if(error instanceof AppError){
+    handleAppError(res , error);
+    return ;
   }
 
   res.status(INTERNAL_SERVER_ERROR).send("Internal server error");
