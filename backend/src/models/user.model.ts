7099965620194f7134bc9d175991c6503b37346b
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-16 22:33:24 +0530

    user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -8,6 +8,7 @@ export interface UserDocument extends mongoose.Document {
     verified:boolean;
     createdAt: Date;
     updatedAt: Date;
+    __v?: number; 
     comparePassword(val:string): Promise<boolean>;
     omitPassword(): Pick<UserDocument, "_id" | "email" | "verified" | "createdAt" | "updatedAt" | "__v">;
 }
@@ -39,6 +40,12 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.comparePassword = async function(val: string){
     return compareValue(val ,this.password)
+}
+
+userSchema.methods.omitPassword = function (){
+    const user = this.toObject();
+    delete user.password;
+    return user;
 }
 
 const User = mongoose.model<UserDocument>('User', userSchema);
