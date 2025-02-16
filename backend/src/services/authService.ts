c6459fb34a5ea9630161dd427b2689e2bfff6b3a
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-16 22:34:02 +0530

    auth service

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,3 +1,12 @@
+import jwt from "jsonwebtoken";
+import VerificationCodeType from "../constants/verificationCodeType";
+import SessionModel from "../models/session.model";
+import User from "../models/user.model";
+import VerificationCodeModel from "../models/verificationCode";
+import { oneYearFromNow } from "../utils/date";
+import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
+import appAssert from "../utils/appAssert";
+import { CONFLICT } from "../constants/http";
 
 
 export type CreateAccountParams = {
@@ -7,4 +16,64 @@ export type CreateAccountParams = {
 }
 export const createAccount = async (data: CreateAccountParams)=> {
     //verify existing user dosn't exist
-}
+    const existingUser = await User.exists({
+        email: data.email,
+    });
+    appAssert(
+        !existingUser, CONFLICT , "Email already in use"
+    );
+
+    // if(existingUser){
+    //     throw new Error("User already exists");
+    // }
+
+    //create user 
+    const user = await User.create({
+        email: data.email,
+        password: data.password,
+    });
+
+    //create verification code
+    const verificationCode = await VerificationCodeModel.create({
+        userId: user._id,
+        type: VerificationCodeType.EmailVerification,
+        expiresAt: oneYearFromNow()
+    })
+
+
+    //create session
+
+    const session = await SessionModel.create({
+        userId: user._id,
+        userAgent: data.userAgent,
+    });
+
+    //sign access token & refresh token
+
+    const refreshToken = jwt.sign(
+        {sessionId: session._id},
+        JWT_REFRESH_SECRET,
+        {
+            audience: ["user"],
+            expiresIn: "30d"
+        }
+    );
+
+    const accessToken = jwt.sign(
+        {
+            userId: user._id,
+            sessionId: session._id},
+        JWT_SECRET,
+        {
+            audience: ["user"],
+            expiresIn: "15m"
+        }
+    );
+
+    //return user & tokens
+    return {
+        user: user.omitPassword(),
+        accessToken,
+        refreshToken,
+    };
+};
