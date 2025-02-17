4962e83dfb75aa14daaa7c46bcd42f8cbe2acd92
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-17 23:52:17 +0530

    updated code

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { userRegister } from "../controllers/auth.controller";
+import { userLogin, userRegister } from "../controllers/auth.controller";
 
 
 
 const authRoutes = Router();
 
 authRoutes.post('/register', userRegister); 
+authRoutes.post('/login', userLogin); 
 
 export default authRoutes;
