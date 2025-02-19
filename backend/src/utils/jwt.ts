e565bce62100b648131fbd9ab4018e3c036f0cb4
Himanshu Tiwari <htiwari578@gmail.com>
2025-02-19 23:46:12 +0530

    updated code

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,36 @@
+import  jwt,{ SignOptions } from "jsonwebtoken";
+import { SessionDocument } from "../models/session.model"
+import { UserDocument } from "../models/user.model";
+import { JWT_SECRET } from "../constants/env";
+
+
+
+
+export type RefreshTokenPayload = {
+    sessionId : SessionDocument["_id"];
+};
+
+export type AccessTokenPayload = {
+    userId: UserDocument["_id"];
+    sessionId: SessionDocument["_id"];
+};
+
+type SignOptionsAndSecret = SignOptions & {
+    secret: string;
+}
+const defaults:  SignOptions = {
+    audience : ["user"],
+}
+const accessTokensSignOptions : SignOptionsAndSecret = {
+    expiresIn : "15m",
+    secret :JWT_SECRET
+}
+
+export const signToken = (
+    payload: AccessTokenPayload | RefreshTokenPayload,
+   options?: SignOptionsAndSecret
+)=> {
+    const {secret, ...signOpts} = options || accessTokensSignOptions ;
+    return jwt.sign(payload , secret, signOpts)
+}  
+   
